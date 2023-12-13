92f4e104f93768308c7f1c2e1d4d33b6bc662a6e
Arindam Karmakar <arindamk410@gmail.com>
2023-12-13 23:37:57 +0530

    SUMMARY: 	1. DependencyService implemented 	2. loginHandler implemented 	3. DatabaseController modified 	4. Updated QuantumUser.fromJson 	5. New utils added (crypto)

--- bin/src/controllers/database_controller.dart
+++ bin/src/controllers/database_controller.dart
@@ -1,10 +1,27 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
+import '../models/quantum_user.dart';
+
 class DatabaseController {
-  final _db = Db("mongodb://localhost:27017/quantum");
+  static final _db = Db("mongodb://localhost:27017/quantum");
+
+  final _userCollection = _db.collection("users");
 
   Future<void> connect() async => await _db.open().then((_) =>
       print("--->>> Database connected :: ${_db.databaseName}@${_db.state}!"));
+
+  Future<bool> addUser(QuantumUser user) async =>
+      (await _userCollection.insertOne(user.toJson())).isSuccess;
+
+  Future<QuantumUser> getUser(String email) async {
+    final user = await _userCollection.findOne(where.eq('email', email));
+
+    if (user != null) {
+      return QuantumUser.fromJson(user);
+    }
+
+    throw Exception("User not found !!!");
+  }
 
   // Init
   static final DatabaseController _shared = DatabaseController._();
--- bin/src/services/utility_service.dart
+++ bin/src/services/utility_service.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
+import 'dart:developer' as dev;
+
+import 'package:crypto/crypto.dart';
 
 import '../enums/resp_type.dart';
+import '../global/constants.dart';
+import '../models/quantum_user.dart';
 
 abstract class Utils {
   static String generateResp({
@@ -19,4 +24,29 @@ abstract class Utils {
 
     return jsonEncode(resp);
   }
+
+  static String generateToken(String data) {
+    final now = DateTime.now();
+
+    final bytes = utf8.encode(Constants.kSalt + data + now.toString());
+    final digest = sha256.convert(bytes);
+
+    return digest.toString();
+  }
+
+  static String encryptPassword(String password) {
+    final key = utf8.encode(Constants.kSalt);
+    final bytes = utf8.encode(password);
+
+    final hmacSha256 = Hmac(sha256, key);
+    final digest = hmacSha256.convert(bytes);
+
+    final encryptedPassword = digest.toString();
+    dev.log("Encrypted password: $encryptedPassword");
+
+    return encryptedPassword;
+  }
+
+  static bool verifyPassword(String password, QuantumUser user) =>
+      encryptPassword(password) == user.password;
 }
